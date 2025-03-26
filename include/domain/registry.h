71420202c47acde4cbe73bf9d5f3e53318814f56
sw <clabil.dev@gmail.com>
2025-03-27 05:42:47 +0900

    [FEAT] Update Register, using base class

--- include/domain/registry.h
+++ include/domain/registry.h
@@ -9,10 +9,11 @@
 
 #include "domain/config.h"
 
+template <typename BASE>
 class GenericRegistry
 {
    public:
-    using Creator = std::function<std::shared_ptr<void>(const Kwargs&)>;
+    using Creator = std::function<std::shared_ptr<BASE>(const Kwargs&)>;
 
     void register_class(const std::string& name, Creator creator)
     {
@@ -23,7 +24,7 @@ class GenericRegistry
         creators_[name] = std::move(creator);
     }
 
-    std::shared_ptr<void> build(const std::string& name, const Kwargs& kwargs) const
+    std::shared_ptr<BASE> build(const std::string& name, const Kwargs& kwargs) const
     {
         auto it = creators_.find(name);
         if (it == creators_.end())
@@ -33,7 +34,7 @@ class GenericRegistry
         return it->second(kwargs);
     }
 
-    std::shared_ptr<void> build(const Kwargs& kwargs) const
+    std::shared_ptr<BASE> build(const Kwargs& kwargs) const
     {
         return build(kwargs["type"].as<std::string>(), kwargs);
     }
@@ -44,27 +45,28 @@ class GenericRegistry
 
 // 네임스페이스별로 Registry 인스턴스를 생성
 // 네임스페이스별로 Registry 인스턴스를 생성
-#define DEFINE_REGISTRY(NAME)               \
-    namespace                               \
-    {                                       \
-    static GenericRegistry NAME##_instance; \
-    }                                       \
-    static GenericRegistry& NAME = NAME##_instance;
+#define DEFINE_REGISTRY(NAME, BASE_CLASS)               \
+    namespace                                           \
+    {                                                   \
+    static GenericRegistry<BASE_CLASS> NAME##_instance; \
+    }                                                   \
+    static GenericRegistry<BASE_CLASS>& NAME = NAME##_instance;
 
 // 자동 등록 매크로
-#define REGISTER(CONCRETE_REGISTRY, ClassType)                                                 \
-    namespace                                                                                  \
-    {                                                                                          \
-    struct AutoRegister_##ClassType                                                            \
-    {                                                                                          \
-        AutoRegister_##ClassType()                                                             \
-        {                                                                                      \
-            CONCRETE_REGISTRY.register_class(#ClassType,                                       \
-                                             [](const Kwargs& kwargs) -> std::shared_ptr<void> \
-                                             { return std::make_shared<ClassType>(kwargs); }); \
-        }                                                                                      \
-    };                                                                                         \
-    static AutoRegister_##ClassType global_auto_##ClassType;                                   \
+#define REGISTER(CONCRETE_REGISTRY, ClassType)                         \
+    namespace                                                          \
+    {                                                                  \
+    struct AutoRegister_##ClassType                                    \
+    {                                                                  \
+        AutoRegister_##ClassType()                                     \
+        {                                                              \
+            CONCRETE_REGISTRY.register_class(                          \
+                #ClassType,                                            \
+                [](const Kwargs& kwargs) -> std::shared_ptr<ClassType> \
+                { return std::make_shared<ClassType>(kwargs); });      \
+        }                                                              \
+    };                                                                 \
+    static AutoRegister_##ClassType global_auto_##ClassType;           \
     }
 
 #endif  // __REGISTRY_H__
--- test/domain/test_registry.cpp
+++ test/domain/test_registry.cpp
@@ -3,28 +3,41 @@
 #include "domain/config.h"
 #include "domain/registry.h"
 
-DEFINE_REGISTRY(MY_REGISTRY);
-
-class MyClassA
+class IMyClass
+{
+   public:
+    virtual ~IMyClass() = default;
+    virtual std::string string() const = 0;
+};
+class MyClassA : public IMyClass
 {
    public:
     MyClassA(const Kwargs& kwargs)
     {
         name_ = kwargs["name"].as<std::string>();
     }
+    std::string string() const override
+    {
+        return name_;
+    }
     std::string name_;
 };
 
-class MyClassB
+class MyClassB : public IMyClass
 {
    public:
     MyClassB(const Kwargs& kwargs)
     {
         value_ = kwargs["value"].as<int>();
     }
+    std::string string() const override
+    {
+        return std::to_string(value_);
+    }
     int value_;
 };
 
+DEFINE_REGISTRY(MY_REGISTRY, IMyClass);
 REGISTER(MY_REGISTRY, MyClassA);
 REGISTER(MY_REGISTRY, MyClassB);
 
@@ -33,14 +46,12 @@ TEST(Registry, BuildAnyClass)
     {
         auto kwargs = Config::parseFromJsonString(R"({"type": "MyClassA", "name": "Alice"})");
         auto ptr = MY_REGISTRY.build(kwargs);
-        auto a = std::static_pointer_cast<MyClassA>(ptr);
-        ASSERT_EQ(a->name_, "Alice");
+        ASSERT_EQ(ptr->string(), "Alice");
     }
 
     {
         auto kwargs = Config::parseFromJsonString(R"({"type": "MyClassB", "value": 42})");
         auto ptr = MY_REGISTRY.build(kwargs);
-        auto b = std::static_pointer_cast<MyClassB>(ptr);
-        ASSERT_EQ(b->value_, 42);
+        ASSERT_EQ(ptr->string(), "42");
     }
 }
